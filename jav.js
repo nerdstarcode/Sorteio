621d908f128a48b3e88d96e7079155325ddf1d82
nerdstarcode <sthivendev@gmail.com>
2022-03-06 14:43:58 -0300

    :bug: Corrigido erro lógico ao sortear o maior numero de 0-100 ao invés de 0-99

--- jav.js
+++ jav.js
@@ -1,5 +1,5 @@
 const Sorteios = {
-    contador: 100,
+    contador: 99,
     numerosParaSortear: [],
     numerosSorteados: [],
     sorteado: 0,
@@ -16,18 +16,22 @@ const Sorteios = {
         function(){
             console.log(this.numerosParaSortear.length);
             this.sorteado = this.getRandomInt(this.contador);
-            this.contador-= 1;
             this.numerosSorteados.push(this.numerosParaSortear[this.sorteado]);
             this.numerosParaSortear.splice(this.sorteado,1);
             console.log(this.numerosSorteados[99 - this.contador]);
             if (this.contador == 0){
                 console.log(this.numerosSorteados);
+                return this.numerosSorteados;
+
             };
+            this.contador-= 1;
             return this.numerosSorteados[99 - this.contador];
+
         }
     ,
     getRandomInt:
         function(max) {
             return Math.floor(Math.random() * max);
         }
+        
 }
